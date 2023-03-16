e0bf8fe9df6148979ea1beb3ecb52ed9a0bf325d
johnnylu3 <lujohnny517@gmail.com>
2023-03-16 10:16:56 -0400

    ca

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/Button.vue'
+import TheWelcome from './components/Card.vue'
 </script>
 
 <template>
