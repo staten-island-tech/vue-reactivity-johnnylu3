b3e6ff02eba98c96f936c4d2c1c061b7842e39a4
johnnylu3 <lujohnny517@gmail.com>
2023-03-22 10:17:14 -0400

    wasd

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,48 +1,15 @@
 <script setup>
-import HelloWorld from './components/Button.vue'
-import TheWelcome from './components/Card.vue'
+import Button from './components/Button.vue'
+import Card from './components/Card.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-    <h1></h1>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    
+  </div>
+  
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
